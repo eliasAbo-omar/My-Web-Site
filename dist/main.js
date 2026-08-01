2a34a8610158c88b278902419aacaaf8d4c404ff
Elias <ekasa123@gmail.com>
2026-08-01 11:00:06 +0300

    New Project With TS

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,3 @@
-const sd = null;
-console.log(sd);
 const headContainer = document.querySelector(".head-container");
 const menu = document.querySelectorAll(".menu");
 const list = ["Home", "About", "Contact", "Blog", "Services"];
@@ -8,15 +6,27 @@ if (!headContainer || !firstMenu || list.length === 0) {
     throw new Error("Element Not Found");
 }
 else {
-    list.forEach((item) => {
+    list.forEach((item, index) => {
         const li = document.createElement("li");
         const a = document.createElement("a");
         a.href = `#${item}`;
         a.textContent = item;
+        if (index === 0) {
+            a.classList.add("active");
+        }
         li.appendChild(a);
         firstMenu.appendChild(li);
         headContainer.appendChild(firstMenu);
     });
 }
+const myAllLinks = document.querySelectorAll(".menu a");
+myAllLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+        myAllLinks.forEach((a) => {
+            a.classList.remove("active");
+        });
+        e.target.classList.add("active");
+    });
+});
 export {};
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 type s = string;
+type n = number;
 
+// ========== Header's Menu & class Active ===========
 const headContainer = document.querySelector(".head-container") as HTMLElement;
 const menu = document.querySelectorAll(".menu") as NodeListOf<HTMLElement>;
 
@@ -10,12 +12,16 @@ const firstMenu = menu[0] as HTMLElement;
 if (!headContainer || !firstMenu || list.length === 0) {
   throw new Error("Element Not Found");
 } else {
-  list.forEach((item: s) => {
+  list.forEach((item: s, index: n) => {
     const li = document.createElement("li") as HTMLLIElement;
     const a = document.createElement("a") as HTMLAnchorElement;
 
     a.href = `#${item}`;
     a.textContent = item;
+
+    if (index === 0) {
+      a.classList.add("active");
+    }
 
     li.appendChild(a);
     firstMenu.appendChild(li);
@@ -23,3 +29,16 @@ if (!headContainer || !firstMenu || list.length === 0) {
     headContainer.appendChild(firstMenu);
   });
 }
+
+const myAllLinks = document.querySelectorAll(
+  ".menu a",
+) as NodeListOf<HTMLAnchorElement>;
+
+myAllLinks.forEach((link: HTMLAnchorElement) => {
+  link.addEventListener("click", (e: PointerEvent) => {
+    myAllLinks.forEach((a: HTMLAnchorElement) => {
+      a.classList.remove("active");
+    });
+    (e.target as HTMLAnchorElement).classList.add("active");
+  });
+});
